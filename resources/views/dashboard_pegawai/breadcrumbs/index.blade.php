293b3e6c5af2e878b529b612116530f1df8f5ca4
aryadzar <112409058+aryadzar@users.noreply.github.com>
2024-10-06 11:34:18 +0700

    Merapihkan html dengan prettier

--- resources/views/dashboard_pegawai/breadcrumbs/index.blade.php
+++ resources/views/dashboard_pegawai/breadcrumbs/index.blade.php
@@ -2,13 +2,12 @@
     <div class=" breadcrumbs text-sm font-bold ">
         <ul class="">
             @foreach ($breadcrumbs as $breadcrumb)
-
                 @if (!is_null($breadcrumb->url) && !$loop->last)
-                    <li class=""><a href="{{ $breadcrumb->url }}"><i class="fa-solid fa-house mr-2"></i> {{ $breadcrumb->title }}</a></li>
+                    <li class=""><a href="{{ $breadcrumb->url }}"><i class="fa-solid fa-house mr-2"></i>
+                            {{ $breadcrumb->title }}</a></li>
                 @else
                     <li class=" font-semibold">{{ $breadcrumb->title }}</li>
                 @endif
-
             @endforeach
         </ul>
     </div>

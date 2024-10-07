42a934f020490b10faabaa28eb921009a64a7ca2
MaharaniWahyuTantri <maharaniwahyutantri525@gmail.com>
2024-10-07 08:58:08 +0700

    scan barcode

---
+++ resources/views/dashboard_pegawai/scan_barcode/index.blade.php
@@ -0,0 +1,18 @@
+@extends('dashboard_pegawai.dashboard-layout.index')
+
+@section('title', 'Dashboard Pegawai')
+@section('nama_pegawai', 'Ahmad Januari')
+@section('role', 'Pegawai')
+@section('content')
+
+    <div class="mb-3">
+        <h1 class="text-2xl font-semibold"><i class="fa-solid fa-qrcode text-blue-500 lg:pr-[10px]"></i><span class=" text-blue-500">
+            Scan Barcode
+        </span></h1>
+
+    </div>
+
+
+
+@endsection
+

8dd8266fea47803e443a79154639ee1a493f633c
aryadzar <112409058+aryadzar@users.noreply.github.com>
2024-10-16 23:26:14 +0700

    validasi barcode menggunakan session

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Models\Presensi;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 use Diglactic\Breadcrumbs\Breadcrumbs;
 use Illuminate\Support\Facades\Session;
@@ -38,7 +39,9 @@ Route::group(['middleware' => 'guest'], function(){
 
 Route::get('/barcode_generate', function (){
     $barcode  = Str::uuid()->toString();
+    $lama_token = now()->addMinutes(10);
 
+    Cache::put("barcode_token_{$barcode}",true,  $lama_token);
     return response()->json(['barcode' => $barcode]);
 
 })->name('barcode_generate');
@@ -60,28 +63,38 @@ Route::group(['middleware' => ['auth']], function(){
         $barcode = $request->input('barcode'); // Ambil barcode yang di-scan
         $userId = Auth::user()->id; // Gunakan ID user yang sedang login
 
-        // Cari presensi berdasarkan ID user dan barcode
-        $alreadyPresent = Presensi::where('id_user', $userId)
-            ->whereDate('tanggal', now()->toDateString())
-            ->exists();
+        if(Cache::has("barcode_token_{$barcode}")){
+            // Cari presensi berdasarkan ID user dan barcode
+            $alreadyPresent = Presensi::where('id_user', $userId)
+                ->whereDate('tanggal', now()->toDateString())
+                ->exists();
 
-        if ($alreadyPresent) {
+            if ($alreadyPresent) {
+                Cache::forget("barcode_token_{$barcode}"); // Hapus token setelah digunakan
+                return response()->json([
+                    'success' => false,
+                    'message' => 'Anda sudah melakukan presensi hari ini.'
+                ]);
+            }
+
+            // Buat data presensi baru
+            $presensi = new Presensi();
+            $presensi->id_user = $userId;
+            $presensi->data_qr_code = $barcode; // Simpan kode random di barcode
+            $presensi->tanggal = now();
+            $presensi->save();
+            Cache::forget("barcode_token_{$barcode}"); // Hapus token setelah digunakan
+
             return response()->json([
-                'success' => false,
-                'message' => 'Anda sudah melakukan presensi hari ini.'
+                'success' => true,
+                'user_name' => Auth::user()->nama
             ]);
+
         }
 
-        // Buat data presensi baru
-        $presensi = new Presensi();
-        $presensi->id_user = $userId;
-        $presensi->data_qr_code = $barcode; // Simpan kode random di barcode
-        $presensi->tanggal = now();
-        $presensi->save();
-
         return response()->json([
-            'success' => true,
-            'user_name' => Auth::user()->nama
+            'success' => false,
+            'message' => 'Barcode tidak valid atau kadaluwarsa.'
         ]);
 
     });
